e493c0b8e465cfd3e9774eee43d68c22769d01ce
Fritz <mls1218@naver.com>
2026-09-08 14:33:24 +0900

    create and write w2s1

---
+++ src/week02/variable01/LongExample.java
@@ -0,0 +1,14 @@
+package week02.variable01;
+
+public class LongExample {
+    public static void main(String[] args) {
+        long var1 = 10;
+        long var2 = 10;
+//        long var3 = 10000000000;
+        long var4 = 10000000000L;
+
+        System.out.println("var1: " + var1);
+        System.out.println("var2: " + var2);
+        System.out.println("var4: " + var4);
+    }
+}
---
+++ src/week02/variable01/promotionExample.java
@@ -0,0 +1,21 @@
+package week02.variable01;
+
+public class promotionExample {
+    public static void main(String[] args) {
+        byte byteValue = 10;
+        int intValue = byteValue;
+        System.out.println("intValue: " + intValue);
+
+        char charValue = '가';
+        intValue = charValue;
+        System.out.println("\'가\'의 유니코드: " +  intValue);
+        intValue = 50;
+        long longValue = intValue;
+        System.out.println("longValue: " + longValue);
+        float floatValue = intValue;
+        System.out.println("floatValue: " + floatValue);
+        floatValue = 100.5f;
+        double doubleValue = floatValue;
+        System.out.println("doubleValue: " + doubleValue);
+    }
+}
